b160e9bb15928957d0062b4de348064083b12f5a
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 12:26:42 +0545

    Image Uploaded On own API server

--- app/src/main/java/com/heros/Register.java
+++ app/src/main/java/com/heros/Register.java
@@ -1,8 +1,14 @@
 package com.heros;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.CharacterPickerDialog;
@@ -12,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -20,18 +27,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import MyAPI.MyAPI;
+import model.ImageResponse;
 import model.User;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import url.Url;
 
 public class Register extends AppCompatActivity {
     private static final String BASE_URL="http://10.0.2.2:3000/";
     private EditText etName,etDescription;
     private Button btnUpload,btnShowDetails;
     private ImageView imgImage;
+    String imagePath;
+    String imageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +57,7 @@ public class Register extends AppCompatActivity {
         btnUpload = findViewById(R.id.btnUpload);
         btnShowDetails = findViewById(R.id.btnShowDetails);
         imgImage = findViewById(R.id.imgImage);
-        loadFromURL();
+//        loadFromURL();
 
         btnShowDetails.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,15 +66,22 @@ public class Register extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        imgImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+        });
 
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SaveImageOnly();
                 RegisterUser();
             }
 
             private  void RegisterUser(){
-                String image = (" ");
+                String image = imageName;
                 String id = (" ");
                 String name = etName.getText().toString();
 
@@ -69,6 +90,7 @@ public class Register extends AppCompatActivity {
                 Map<String,String> map = new HashMap<>();
                 map.put("name",name);
                 map.put("desc",desc);
+                map.put("image",image);
 
                 User user = new User(image,id,name,desc);
 
@@ -77,7 +99,7 @@ public class Register extends AppCompatActivity {
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
 
-                 MyAPI myAPI = retrofit.create(MyAPI.class);
+                 MyAPI myAPI = Url.getInstance().create(MyAPI.class);
 
                 Call<Void> voidCall = myAPI.addHero2(map);
 
@@ -98,22 +120,80 @@ public class Register extends AppCompatActivity {
                 });
 
             }
+
+
+
         });
+
     }
-    private void StrictMode(){
+    private void BrowseImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+            if(data == null){
+                Toast.makeText(this,"PLEASE SELECT AN IMAGE",Toast.LENGTH_LONG).show();
+            }
+        }
+        Uri uri = data.getData();
+        imagePath=getRealPathFromUri(uri);
+        previewImage(imagePath);
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(),uri,projection,null,null,null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
+    }
+    private void previewImage(String imagePath){
+        File imgFile = new File(imagePath);
+        if(imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            imgImage.setImageBitmap(myBitmap);
+        }
+    }
+        private void StrictMode(){
         android.os.StrictMode.ThreadPolicy policy =
                 new android.os.StrictMode.ThreadPolicy.Builder().permitAll().build();
         android.os.StrictMode.setThreadPolicy(policy);
 
     }
-    private void loadFromURL(){
-        StrictMode();
-        try {
-            String imgURL = "http://10.0.2.2:3000//uploads/flower.jpeg";
-            URL url = new URL(imgURL);
-            imgImage.setImageBitmap(BitmapFactory.decodeStream((InputStream)url.getContent()));
-        }  catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void SaveImageOnly(){
+        File file = new File(imagePath);
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"),file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("imageFile",file.getName(),requestBody);
+
+        MyAPI myAPI = Url.getInstance().create(MyAPI.class);
+       Call<ImageResponse> responseCall = myAPI.uploadImage(body);
+       StrictMode();
+       try {
+           Response<ImageResponse> imageResponseResponse = responseCall.execute();
+           imageName = imageResponseResponse.body().getFilename();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
     }
+//    private void loadFromURL(){
+//        StrictMode();
+//        try {
+//            String imgURL = "http://10.0.2.2:3000//uploads/flower.jpeg";
+//            URL url = new URL(imgURL);
+//            imgImage.setImageBitmap(BitmapFactory.decodeStream((InputStream)url.getContent()));
+//        }  catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
